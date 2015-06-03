b4f529592e35b79c1ac4f738fa055f1c448aeb16
Dante Natoli <Dante_Natoli@intuit.com>
2015-06-03 08:26:15 -0700

    Changed Map to use 2-D array of tiles instead of 1-D for easier access at (x, y) coordinates

--- Practice Game/Map.swift
+++ Practice Game/Map.swift
@@ -15,61 +15,25 @@ enum Walkability {
 
 class MapTile: NSObject {
   var walkability: Walkability
-//  var index: Int
   var left: MapTile?, right: MapTile?, up: MapTile?, down: MapTile?
+  let x: Int, y: Int
   weak var map: Map?
   
-  init(walk: Walkability, index: Int) {
+  init(walk: Walkability, x: Int, y: Int) {
     walkability = walk
-//    self.index = index
+    self.x = x
+    self.y = y
   }
   
   func isWalkable() -> Bool {
     return walkability != .Obstacle
   }
-}
-
-class Map: NSObject {
-  var allTiles: [MapTile]
-  let height: Int, width: Int
   
-  init(height: Int, width: Int) {
-    self.height = height
-    self.width = width
-    allTiles = [MapTile]()
-  }
-  
-  // Builds the game map. Tiles at indeces contained in NOWALK will have obstacles, and the rest will be normal.
-  func populateMap(noWalk: [Int] = []) {
-    let numSquares = height * width
-    for i in 0..<numSquares {
-      let newTile = MapTile(walk: .Normal, index: i)
-      if contains(noWalk, i) {
-        newTile.walkability = .Obstacle
-      }
-      newTile.map = self
-      if i / height != 0 {
-        newTile.up = allTiles[i - height]
-        allTiles[i - height].down = newTile
-      }
-      if i % width != 0 {
-        newTile.left = allTiles[i - 1]
-        allTiles[i - 1].right = newTile
-      }
-      allTiles.append(newTile)
-    }
-  }
-  
-  // Calculates the number of spaces between two tiles on the game map.
-//  func calcDist(tile1: MapTile, tile2: MapTile) -> Int {
-//    return abs((tile1.index % width) - (tile2.index % width)) + abs((tile1.index / height) - (tile2.index / height));
-//  }
-  
-  /* Uses breadth-first search to return the range a character can move from SPACE using no more than NUMMOVES moves.
-   * Takes obstacles into account. */
-  func reachable(space: MapTile, numMoves: Int) -> [MapTile] {
+  /* Uses breadth-first search to return the range a character can move from this space using
+   * no more than NUMMOVES moves. Takes obstacles into account. */
+  func reachable(numMoves: Int) -> [MapTile] {
     var range = [MapTile]()
-    range.append(space)
+    range.append(self)
     var level = 0
     var count = 0
     var curr = 0
@@ -93,6 +57,31 @@ class Map: NSObject {
       ++level
     }
     return range
+  }
+}
+
+class Map: NSObject {
+  var allTiles: [[MapTile]]
+  let height: Int, width: Int
+  
+  init(height: Int, width: Int) {
+    self.height = height
+    self.width = width
+    allTiles = [[MapTile]]()
+  }
+  
+  // Builds the game map. Tiles at points contained in NOWALK will have obstacles, and the rest will be normal.
+  func populateMap(noWalk: [(Int, Int)] = []) {
+    for i in 0..<width {
+      allTiles.append([MapTile]())
+      for j in 0..<height {
+        let newTile = MapTile(walk: .Normal, x: i, y: j)
+        allTiles[i].append(newTile)
+      }
+    }
+    for (x, y) in noWalk {
+      allTiles[x][y].walkability = .Obstacle
+    }
   }
 }
 
